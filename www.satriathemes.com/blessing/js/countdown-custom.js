163f94a18629cb8b686153cfaf00d8ef3e0e8f67
BRAIYANM <40458784+BRAIYANM@users.noreply.github.com>
2019-01-03 09:07:25 +0300

    Initial commit

---
+++ www.satriathemes.com/blessing/js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2017, 3-1, 10, 8)}); // year, month, date, hour
+        });
+});		
+
